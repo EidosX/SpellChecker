3ac3ee5c8d8b49eed5cf16c20e71e3d978460f99
Diego Imbert <diegoimbert@pm.me>
2021-09-16 11:21:43 +0200

    Two rows levenshtein distance implementation

---
+++ src/levenshtein/TwoRowsLevenshtein.java
@@ -0,0 +1,28 @@
+package levenshtein;
+
+import static tools.MathTools.*;
+
+public class TwoRowsLevenshtein implements Levenshtein {
+  @Override
+  public int distance(CharSequence str1, CharSequence str2) {
+    int[] distances = new int[str1.length()];
+    int[] temp = new int[str1.length()];
+
+    for (int j = 0; j < str2.length(); ++j) {
+      for (int i = 0; i < str1.length(); ++i) {
+        int topLeft = i > 0 && j > 0 ? distances[i - 1] : max(i, j);
+        if (str1.charAt(i) == str2.charAt(j)) {
+          temp[i] = topLeft;
+        } else {
+          int top = j > 0 ? distances[i] : i;
+          int left = i > 0 ? temp[i - 1] : j;
+          temp[i] = 1 + min(top, left, topLeft);
+        }
+      }
+      int[] temp2 = temp;
+      temp = distances;
+      distances = temp2;
+    }
+    return distances[str1.length() - 1];
+  }
+}
